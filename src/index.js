615a456e9549b07ddef4aa3aadf2f27c1660c905
Mykyta Slipachuk <nikita.slipachuk@gmail.com>
2024-03-05 13:48:15 +0100

    testv4

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import  App  from 'components/App';
 import { ThemeProvider } from 'components/ThemeContext';
 import './index.css';
-import TestApp from "./components/TestApp"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
-      {/* <TestApp/> */}
     </ThemeProvider>
   </React.StrictMode>
 );
